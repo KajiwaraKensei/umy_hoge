ce171a3a6cd8f8a84630d18373f635e7d0b88eaf
Kajiwara Kensei <kajiwara.k@icraft.jp>
2021-11-24 18:26:47 +0900

    機能追加

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from "react";
 import "./App.css";
 import Confirm from "./confirm/select";
 import Sub from "./confirm/sub";
-
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
 import {
   getFirestore,
   collection,
   getDocs,
+  limit,
   query,
-  startAfter,
+  doc,
+  updateDoc,
 } from "firebase/firestore";
 
 import "@fontsource/roboto/300.css";
@@ -16,23 +19,21 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
-// async function update(id, data) {
-//   const db = getFirestore();
-
-//   const ref = doc(db, "account", "J39OYR3xpzUwqU21DihQ2tc1Inu2");
-//   return updateDoc(ref, data);
-// }
-async function get(docName, lastVisible) {
+async function update(id, docName, data) {
+  const db = getFirestore();
+  const ref = doc(db, docName, id);
+  delete data.id;
+  return updateDoc(ref, data);
+}
+async function get(docName, limitCount) {
   const db = getFirestore();
   const list = [];
-  const params = [collection(db, docName)];
-  lastVisible && params.push(startAfter(lastVisible));
+  const params = [collection(db, docName), limit(limitCount)];
   const first = query(...params);
   const querySnapshot = await getDocs(first);
   querySnapshot.forEach((doc) => {
     list.push({ id: doc.id, ...doc.data() });
   });
-  console.log(list);
   const nextVisible = querySnapshot.docs[querySnapshot.docs.length - 1];
   return { list, lastVisible: nextVisible };
 }
@@ -42,20 +43,23 @@ function App() {
   const [toggle, setToggle] = React.useState(true);
   const [docName, setDocName] = React.useState("");
   const [loading, setLoading] = React.useState(false);
-  const [lastVisible, setLastVisible] = React.useState();
-  React.useEffect(() => {
+  const [limit, setLimit] = React.useState(5);
+
+  const GetData = () => {
     docName.length && setLoading(true);
     docName.length &&
-      get(docName)
+      get(docName, limit - 0)
         .then((res) => {
           setLoading(false);
           setData(res.list);
-          setLastVisible(res.lastVisible);
         })
         .catch((err) => {
           setLoading(false);
           alert(err);
         });
+  };
+  React.useEffect(() => {
+    GetData();
   }, [docName]);
 
   const close = () => {
@@ -70,12 +74,72 @@ function App() {
     setDocName(nextDocName);
   };
 
+  const handleSave = (saveData) => {
+    console.log(saveData);
+    update(saveData.id, docName, saveData);
+  };
+
+  const createUpdateData = (keys, value, target) => {
+    const key = keys.shift();
+    if (keys.length) {
+      return {
+        ...target,
+        [key]: createUpdateData(keys, value, target[key]),
+      };
+    }
+    if (!isNaN(key)) {
+      let returnData = [];
+      Object.keys(target).forEach((index) => {
+        const valueData = index === key ? value : target[index];
+        returnData.push(valueData);
+      });
+      return returnData;
+    }
+
+    return {
+      ...target,
+      [key]: value,
+    };
+  };
+  const handleChangeData = (event) => {
+    event.keyNames.shift();
+    let nextData = JSON.parse(JSON.stringify(list));
+    const targetIndex = event.keyNames.shift();
+    const target = list[targetIndex];
+    const next = createUpdateData(event.keyNames, event.value, target);
+    nextData[targetIndex] = next;
+    setData(nextData);
+  };
+
+  const handleChangeLimit = (event) => {
+    setLimit(event.target.value);
+  };
+
   return (
     <div className="App">
       <div>{loading && "通信中"}</div>
       <h2>{docName}</h2>
-      {!toggle && <button onClick={open}>変更</button>}
-      <Sub data={list} />
+      {!toggle && <Button onClick={open}>コレクション変更</Button>}
+
+      <div>
+        {!toggle && (
+          <>
+            <TextField
+              onChange={handleChangeLimit}
+              label="limit"
+              value={limit}
+            ></TextField>
+            <Button onClick={GetData}>変更</Button>
+          </>
+        )}
+      </div>
+
+      <Sub
+        data={list}
+        onSave={handleSave}
+        change={handleChangeData}
+        keyName="/"
+      />
       {toggle && (
         <Confirm
           init={docName}
--- src/confirm/sub.js
+++ src/confirm/sub.js
@@ -1,19 +1,56 @@
 import React from "react";
-import TextField from "@mui/material/TextField";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Switch from "@mui/material/Switch";
 import Input from "@mui/material/Input";
 import FormControl from "@mui/material/FormControl";
 import InputLabel from "@mui/material/InputLabel";
+import Button from "@mui/material/Button";
 
 const Component = (props) => {
+  const handleChange = (keyName, type) => (event) => {
+    const params = {
+      boolean: event.target.checked,
+      string: event.target.value,
+      number: event.target.value - 0,
+    };
+    const value = params[type];
+    props.change({
+      keyNames: [props.keyName, keyName],
+      value,
+      type,
+    });
+  };
+
+  const childChange = (child) => {
+    props.change({
+      keyNames: [props.keyName, ...child.keyNames],
+      value: child.value,
+      type: child.type,
+    });
+  };
+
+  const handleSave = (key) => () => {
+    props.onSave(props.data[key]);
+  };
+
   const mapData = Object.keys(props.data).map((key) => {
-    const type = typeof props.data[key];
-    if (type === "object") {
+    const type = Array.isArray(props.data[key])
+      ? "array"
+      : typeof props.data[key];
+    if (type === "object" || type === "array") {
       return (
         <div className="map_i_value" key={key}>
           <p>{key}</p>
-          <Component data={props.data[key]}></Component>
+          <Component
+            data={props.data[key]}
+            keyName={key}
+            change={childChange}
+          ></Component>
+          {props.keyName === "/" && (
+            <Button variant="outlined" onClick={handleSave(key)}>
+              更新
+            </Button>
+          )}
         </div>
       );
     }
@@ -22,7 +59,11 @@ const Component = (props) => {
         <div className="map_i_value" key={key}>
           <FormControl fullWidth sx={{ m: 1 }} variant="standard">
             <InputLabel htmlFor={"standard-" + key}>{key}</InputLabel>
-            <Input id={"standard-" + key} value={props.data[key]} />
+            <Input
+              onChange={handleChange(key, type)}
+              id={"standard-" + key}
+              value={props.data[key]}
+            />
           </FormControl>
         </div>
       );
@@ -35,10 +76,12 @@ const Component = (props) => {
             control={<Switch checked={props.data[key]} />}
             label={key}
             labelPlacement="start"
+            onChange={handleChange(key, type)}
           />
         </div>
       );
     }
+    return null;
   });
   return <div>{mapData}</div>;
 };
